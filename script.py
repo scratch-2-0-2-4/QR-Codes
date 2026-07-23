5ec40d9250d64d4c3a06be9674b4cb29e60ab4cd
Scratch_2_0_2_4 <scratch.20.24.0@gmail.com>
2026-07-23 10:59:40 +0200

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ def run():
         import qrcode
     except ImportError:
         print("❌ qrcode non installé!")
-        print("Installe avec: pip install qrcode[pil]")
+        print("Installe avec: pip3 install \"qrcode[pil]\"")
         return
     
     while True:
@@ -17,7 +17,17 @@ def run():
             print("Au revoir !")
             break
         
-        qr = qrcode.QRCode(version=1, box_size=10, border=4)
+        # Couleurs personnalisables
+        fill_color = input("Couleur du QR (noir/bleu/rouge) [défaut: noir] >>> ") or "black"
+        back_color = input("Couleur du fond (blanc/jaune/vert) [défaut: blanc] >>> ") or "white"
+        
+        qr = qrcode.QRCode(
+            version=1,
+            box_size=10,
+            border=4,
+            fill_color=fill_color,  # Couleur du QR
+            back_color=back_color   # Couleur du fond
+        )
         qr.add_data(url)
         qr.make(fit=True)
         qr.print_ascii(invert=True)
